cfadcc0c2b7d2c681babfd2bd3e12ec45f89daee
Berjoyner <joyner.amber@gmail.com>
2014-04-28 13:57:08 -0400

    Initial Commit

--- app/arrays.js
+++ app/arrays.js
@@ -1,3 +1,5 @@
+//testing: 
+
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
--- app/config.js
+++ app/config.js
@@ -1,3 +1,5 @@
+//testing:
+
 // Set the require.js configuration for your application.
 require.config({
   // Initialize the application with the main application file
